68a7b8311b3b520c2e77948d6d597f8387070e47
Bartosz Bonisławski <b.bonislawski@hotmail.com>
2017-10-28 17:30:37 +0200

    temporary allow 3.5.0 alpha to render segment initializer

--- app/overrides/add_segment_initializer_to_layout.rb
+++ app/overrides/add_segment_initializer_to_layout.rb
@@ -1,5 +1,5 @@
 spree_version = Gem.loaded_specs['spree_core'].version
-unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Version.create('3.5.0')
+unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Version.create('3.5.0') && spree_version != Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
     virtual_path: 'spree/layouts/spree_application',
     name: 'add_segment_initializer_to_layout',
