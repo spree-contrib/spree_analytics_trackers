53a42b670d83d8896d7d77ca94b6dfda72180e49
Damian Legawiec <damian@sparksolutions.co>
2018-01-21 14:36:32 +0100

    Fixed migration when engine column is already defined

--- db/migrate/20171013160337_create_spree_trackers.rb
+++ db/migrate/20171013160337_create_spree_trackers.rb
@@ -3,8 +3,10 @@ class CreateSpreeTrackers < SpreeExtension::Migration[4.2]
     if table_exists?(:spree_trackers)
       add_index :spree_trackers, :active unless index_exists?(:spree_trackers, :active)
       remove_column :spree_trackers, :environment if column_exists?(:spree_trackers, :environment)
-      add_column :spree_trackers, :kind, :integer, default: 0, null: false, index: true unless column_exists?(:spree_trackers, :kind)
-      rename_column :spree_trackers, :kind, :engine if column_exists?(:spree_trackers, :kind)
+      unless column_exists?(:spree_trackers, :engine)
+        add_column :spree_trackers, :kind, :integer, default: 0, null: false, index: true unless column_exists?(:spree_trackers, :kind)
+        rename_column :spree_trackers, :kind, :engine if column_exists?(:spree_trackers, :kind)
+      end
     else
       create_table :spree_trackers do |t|
         t.string 'analytics_id'
