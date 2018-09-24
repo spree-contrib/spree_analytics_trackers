a24430a3302b094bff01ff365486b0228ae20aa7
Alexey Topolyanskiy <devilcoders@protonmail.ch>
2018-09-24 12:36:52 +0200

    Move trackers to sidebar config menu. Fix bootstrap markup.

--- app/overrides/add_analytics_to_admin_sidebar.rb
+++ app/overrides/add_analytics_to_admin_sidebar.rb
@@ -1,9 +1,8 @@
 if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0')
   Deface::Override.new(
     virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-    name: 'add_analytics_to_admin_configurations_sidebar_menu',
+    name: 'add_analytics_to_admin_sidebar',
     insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-    original: '72f6a33afb2627a467ae458d42991ab0ce4cc6df',
-    text: '<%= configurations_sidebar_menu_item(Spree.t(:analytics_trackers), spree.admin_trackers_path) if can? :manage, Spree::Tracker %>'
+    partial: 'spree/admin/shared/analytics_sidebar_menu'
   )
 end
