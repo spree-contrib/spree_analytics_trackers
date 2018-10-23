94f3e3b685d9fdfbfcb270188020dfe4967db430
Matthew Kennedy <m.kennedy@me.com>
2018-10-23 10:17:40 +0100

    squashed commits into one

--- app/helpers/spree/trackers_helper.rb
+++ app/helpers/spree/trackers_helper.rb
@@ -7,15 +7,15 @@ module Spree
         category: product.category.try(:name),
         name: product.name,
         brand: product.brand.try(:name),
-        price: product.price,
+        price: product.price.to_f,
         currency: product.currency,
         url: product_url(product),
       }.tap do |hash|
         # This method returns either asset_url (Paperclip) or rails_blob_path (ActiveStorage) to return the specified image(s)
         if defined?(ActiveStorage) && !Rails.application.config.use_paperclip
-          hash[:image_url] = Rails.application.routes.url_helpers.rails_blob_path(optional.delete(:image).attachment, only_path: true) if optional[:image]
+          hash[:image_url] =  request.base_url + Rails.application.routes.url_helpers.rails_blob_path(optional.delete(:image).attachment, only_path: true) if optional[:image]
         else
-          hash[:image_url] = asset_url(optional.delete(:image).attachment) if optional[:image]
+          hash[:image_url] = request.base_url + asset_url(optional.delete(:image).attachment) if optional[:image]
         end
       end.merge(optional).to_json.html_safe
     end
--- app/overrides/add_analytics_to_admin_sidebar.rb
+++ app/overrides/add_analytics_to_admin_sidebar.rb
@@ -1,8 +1,8 @@
 if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0')
   Deface::Override.new(
-    virtual_path: 'spree/layouts/admin',
-    name: 'add_analytics_to_admin_sidebar',
-    insert_bottom: '#main-sidebar',
-    partial: 'spree/admin/shared/analytics_sidebar_menu'
+    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
+    name: 'add_analytics_to_admin_configurations_sidebar_menu',
+    insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
+    text: '<%= configurations_sidebar_menu_item(Spree.t(:analytics_trackers), spree.admin_trackers_path) if can? :manage, Spree::Tracker %>'
   )
 end
