ae6ed7c733340d6b81a22375b1961090cb83326c
Damian Legawiec <damian@sparksolutions.co>
2019-02-03 22:49:57 +0100

    Fixed factories for Factory Bot 5.0

--- lib/spree_analytics_trackers/factories.rb
+++ lib/spree_analytics_trackers/factories.rb
@@ -1,9 +1,9 @@
 unless FactoryBot.factories.registered?(:tracker)
   FactoryBot.define do
     factory :tracker, class: Spree::Tracker do
-      analytics_id 'A100'
-      active true
-      engine :google_analytics
+      analytics_id { 'A100' }
+      active { true }
+      engine { :google_analytics }
     end
   end
 end
