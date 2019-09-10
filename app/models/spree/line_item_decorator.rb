d32356d8c9d7f915b2912aed613299dddbcb29fa
Damian Legawiec <damian@sparksolutions.co>
2019-09-10 10:53:30 +0200

    Drop support for Spree versions older than 3.5

--- app/models/spree/line_item_decorator.rb
+++
@@ -1,6 +0,0 @@
-if Gem.loaded_specs['spree_core'].version < Gem::Version.create('3.2.0')
-  Spree::LineItem.class_eval do
-    delegate :brand, :category, to: :product
-    delegate :options_text, to: :variant
-  end
-end
--- app/models/spree/order_decorator.rb
+++
@@ -1,7 +0,0 @@
-if Gem.loaded_specs['spree_core'].version < Gem::Version.create('3.3.0')
-  Spree::Order.class_eval do
-    def promo_code
-      promotions.pluck(:code).compact.first
-    end
-  end
-end
