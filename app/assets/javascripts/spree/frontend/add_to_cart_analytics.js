d4a98b9b435ba05b73e3b378eeca9c9c3742ebaf
Punkbooster <ugliar.arsen@gmail.com>
2019-10-24 14:47:14 +0200

    Remove unused add_to_cart partials

--- app/assets/javascripts/spree/frontend/add_to_cart_analytics.js
+++ app/assets/javascripts/spree/frontend/add_to_cart_analytics.js
@@ -1,45 +1,44 @@
 //= require spree/frontend
 
-(function($) {
-  $(document).ready(function(){
-    $('body').on('product_add_to_cart', function(event) {
-      var variant = event.variant
-      var quantity = event.quantity_increment
+Spree.ready(function(){
+  $('body').on('product_add_to_cart', function(event) {
+    var variant = event.variant
+    var quantity = event.quantity_increment
+    var currency = event.cart.currency
 
-      if (typeof gtag !== 'undefined') {
-        gaAddToCart(variant, quantity)
-      } else if (typeof analytics !== 'undefined') {
-        segmentAddtoCart(variant, quantity)
-      }
-    })
-
-    function gaAddToCart(variant, quantity) {
-      gtag(
-        'event',
-        'add_to_cart',
-        {
-          id: variant.sku,
-          name: variant.name,
-          category: variant.category,
-          variant: variant.options_text,
-          brand: variant.brand,
-          price: variant.price,
-          quantity: quantity
-        }
-      );
-    }
-
-    function segmentAddtoCart(variant, quantity) {
-      analytics.track('Product Added', {
-        product_id: variant.id,
-        sku: variant.sku,
-        category: variant.category,
-        name: variant.name,
-        brand: variant.brand,
-        price: variant.price,
-        currency: variant.currency,
-        quantity: quantity
-      });
+    if (typeof gtag !== 'undefined') {
+      gaAddToCart(variant, quantity)
+    } else if (typeof analytics !== 'undefined') {
+      segmentAddtoCart(variant, quantity)
     }
   })
-})(jQuery);
+
+  function gaAddToCart(variant, quantity) {
+    gtag(
+      'event',
+      'add_to_cart',
+      {
+        id: variant.sku,
+        name: variant.name,
+        category: variant.category,
+        variant: variant.options_text,
+        brand: variant.brand,
+        price: variant.price,
+        quantity: quantity
+      }
+    );
+  }
+
+  function segmentAddtoCart(variant, quantity, currency) {
+    analytics.track('Product Added', {
+      product_id: variant.id,
+      sku: variant.sku,
+      category: variant.category,
+      name: variant.name,
+      brand: variant.brand,
+      price: variant.price,
+      currency: currency,
+      quantity: quantity
+    });
+  }
+});
--- app/overrides/add_google_add_to_cart.rb
+++
@@ -1,8 +0,0 @@
-if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
-  Deface::Override.new(
-    virtual_path: 'spree/orders/edit',
-    name: 'add_google_add_to_cart',
-    insert_top: "[data-hook='cart_container']",
-    partial: 'spree/shared/trackers/google_analytics/add_to_cart.js'
-  )
-end
