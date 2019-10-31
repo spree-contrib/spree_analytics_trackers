ab333392dc77e1768d447f3b91e539ae30951842
Punkbooster <ugliar.arsen@gmail.com>
2019-10-31 12:30:10 +0100

    Add segment initializer on checkout layout

--- app/overrides/add_segment_initializer_to_layout.rb
+++ app/overrides/add_segment_initializer_to_layout.rb
@@ -4,7 +4,13 @@ unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Ver
     virtual_path: 'spree/layouts/spree_application',
     name: 'add_segment_initializer_to_layout',
     insert_top: "[data-hook='body']",
-    partial: 'spree/shared/trackers/segment/initializer.js',
-    original: '26b91c1073b75c93c9e9c89b1f0a6f52ef29a8f9'
+    partial: 'spree/shared/trackers/segment/initializer.js'
+  )
+
+  Deface::Override.new(
+    virtual_path: 'spree/layouts/checkout',
+    name: 'add_segment_initializer_to_checkout',
+    insert_top: "[data-hook='body']",
+    partial: 'spree/shared/trackers/segment/initializer.js'
   )
 end
