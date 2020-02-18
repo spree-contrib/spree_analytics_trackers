31f91d0b80d76dd198e33fc3d7d96e3c1c2df81f
Punkbooster <ugliar.arsen@gmail.com>
2020-02-18 17:20:29 +0100

    Remove unnecessary turbolinks:load listeners & fix some deface namings

--- app/overrides/add_google_analytics_page_view_to_spree_application.rb
+++ app/overrides/add_google_analytics_page_view_to_spree_application.rb
@@ -2,7 +2,7 @@ spree_version = Gem.loaded_specs['spree_core'].version
 unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Version.create('3.5.0') && spree_version != Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
     virtual_path: 'spree/layouts/spree_application',
-    name: 'add_google_analytics_page_tracker_to_body',
+    name: 'add_google_analytics_page_viewed_to_spree_application',
     insert_bottom: "[data-hook='body']",
     partial: 'spree/shared/trackers/google_analytics/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
@@ -10,7 +10,7 @@ unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Ver
 
   Deface::Override.new(
     virtual_path: 'spree/layouts/checkout',
-    name: 'add_google_analytics_page_tracker_to_spree_checkout',
+    name: 'add_google_analytics_page_viewed_to_spree_checkout',
     insert_bottom: "[data-hook='body']",
     partial: 'spree/shared/trackers/google_analytics/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -1,7 +1,7 @@
 if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
     virtual_path: 'spree/layouts/spree_application',
-    name: 'add_google_analytics_to_spree_application',
+    name: 'add_google_analytics_initializer_to_spree_application',
     insert_top: '[data-hook="inside_head"]',
     partial: 'spree/shared/trackers/google_analytics/initializer.js',
     original: 'cfa30a2831d9a41394c03229cd28b3c7eee69585'
@@ -9,7 +9,7 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
 
   Deface::Override.new(
     virtual_path: 'spree/layouts/checkout',
-    name: 'add_google_analytics_to_spree_checkout',
+    name: 'add_google_analytics_initializer_to_spree_checkout',
     insert_top: '[data-hook="inside_head"]',
     partial: 'spree/shared/trackers/google_analytics/initializer.js',
     original: 'ca3d46a134b93856baf937cf2e473da66d9d8efc'
