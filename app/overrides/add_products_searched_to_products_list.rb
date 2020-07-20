1d85d86d2700489ebe27286134b338817108e48b
Robert Nowakowski <aplegatt@gmail.com>
2020-07-20 16:31:20 +0200

    Segment Products Searched event fix

--- app/overrides/add_products_searched_to_products_list.rb
+++ app/overrides/add_products_searched_to_products_list.rb
@@ -1,9 +1,9 @@
 spree_version = Gem.loaded_specs['spree_core'].version
 unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Version.create('3.5.0')
   Deface::Override.new(
-    virtual_path: 'spree/shared/_products',
+    virtual_path: 'spree/products/index',
     name: 'add_products_searched_to_products_list',
-    insert_before: '[data-hook="products_search_results_heading"]',
+    insert_before: '[data-hook="search_results"]',
     partial: 'spree/shared/trackers/segment/products_searched.js',
     original: '8b47dddca9dfaaacca3d462459c6c1ef06c09926' 
   )
