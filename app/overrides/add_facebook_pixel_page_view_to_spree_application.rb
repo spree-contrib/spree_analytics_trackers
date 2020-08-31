b384c4f2291b211743e60fd2efb05f36927a2f8f
Maximiliano López <maxi.lopez.ramirez@gmail.com>
2020-08-31 04:58:21 -0400

    Add facebook pixel in checkout

--- app/overrides/add_facebook_pixel_page_view_to_spree_application.rb
+++ app/overrides/add_facebook_pixel_page_view_to_spree_application.rb
@@ -7,4 +7,12 @@ unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Ver
     partial: 'spree/shared/trackers/facebook_pixel/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
   )
+
+  Deface::Override.new(
+    virtual_path: 'spree/layouts/checkout',
+    name: 'add_facebook_pixel_page_viewed_to_spree_checkout',
+    insert_top: "[data-hook='body']",
+    partial: 'spree/shared/trackers/facebook_pixel/page_viewed.js',
+    original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
+  )
 end
