fe861f3c3923d0cfed853b8b9fcab71d2d551d02
Nathaniel Nande <nathaniel.nande@sparksolutions.co>
2020-12-16 11:22:48 +0100

    Added Spree::Tracker validation on active field - only one active tracker per store permitted

--- app/models/spree/tracker.rb
+++ app/models/spree/tracker.rb
@@ -7,6 +7,7 @@ module Spree
 
     validates :analytics_id, presence: true, uniqueness: { scope: [:engine, :store_id], case_sensitive: false }
     validates :store, presence: true
+    validate :ensure_one_active_tracker_assigned_to_store, if: -> { (new_record? || active_changed?) && active? }
 
     scope :active, -> { where(active: true) }
 
@@ -26,6 +27,23 @@ module Spree
       TRACKING_ENGINES.each do |engine|
         Rails.cache.delete("current_tracker/#{engine}/#{store_id}")
       end
+    end
+
+    private
+
+    def ensure_one_active_tracker_assigned_to_store
+      if store_has_other_active_trackers?
+        errors.add(
+          :active,
+          I18n.t('activemodel.errors.models.spree/tracker.attributes.active.store_has_already_assigned_tracker',
+            store_name: store.name
+          )
+        )
+      end
+    end
+
+    def store_has_other_active_trackers?
+      Spree::Tracker.active.where.not(id: id).where(store_id: store_id).exists?
     end
   end
 end
