578113a04af8e3bbdd618f1320a3e84552ac4562
Maximiliano López <maxi.lopez.ramirez@gmail.com>
2021-05-11 11:24:17 -0400

    bump version

--- lib/spree_analytics_trackers/version.rb
+++ lib/spree_analytics_trackers/version.rb
@@ -10,7 +10,7 @@ module SpreeAnalyticsTrackers
   module VERSION
     MAJOR = 2
     MINOR = 1
-    TINY  = 13
+    TINY  = 14
     PRE   = 'rc1'
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
