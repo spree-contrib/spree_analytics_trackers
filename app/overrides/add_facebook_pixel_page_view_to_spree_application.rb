ce81666e9bf3de579a4a478623faf81094faa4cd
chinoxchen <chienfu.udp@gmail.com>
2021-06-15 22:07:06 -0400

    Changes initializers and page view events overrides

--- app/overrides/add_facebook_pixel_page_view_to_spree_application.rb
+++ app/overrides/add_facebook_pixel_page_view_to_spree_application.rb
@@ -1,17 +1,9 @@
 spree_version = Gem.loaded_specs['spree_core'].version
 unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Version.create('3.5.0') && spree_version != Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
-    virtual_path: 'spree/layouts/spree_application',
+    virtual_path: 'spree/shared/_head',
     name: 'add_facebook_pixel_page_view_to_spree_application',
-    insert_top: "[data-hook='inside_head']",
-    partial: 'spree/shared/trackers/facebook_pixel/page_viewed.js',
-    original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
-  )
-
-  Deface::Override.new(
-    virtual_path: 'spree/layouts/checkout',
-    name: 'add_facebook_pixel_page_viewed_to_spree_checkout',
-    insert_top: "[data-hook='inside_head']",
+    insert_before: 'meta',
     partial: 'spree/shared/trackers/facebook_pixel/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
   )
