c043b3e624d8bb3a380b7d99e70752d6bfe89bf1
Felipe Monsalves Sena <fm.monsalves@gmail.com>
2021-07-12 19:20:48 -0400

    update insert before to avoid duplicates

--- app/overrides/add_segment_page_viewed_to_layout.rb
+++ app/overrides/add_segment_page_viewed_to_layout.rb
@@ -3,7 +3,7 @@ unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Ver
   Deface::Override.new(
     virtual_path: 'spree/shared/_head',
     name: 'add_segment_page_tracker_to_body',
-    insert_before: 'meta',
+    insert_before: "[name='facebook-domain-verification']",
     partial: 'spree/shared/trackers/segment/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
   )
