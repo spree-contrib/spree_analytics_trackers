5fddcdddfc75fc99f45888b64a62704fcff8572e
Michael Davidson <mdavo6@gmail.com>
2021-07-22 10:54:16 +1000

    Update defined? argument

--- app/controllers/spree_analytics_trackers/store_controller_decorator.rb
+++ app/controllers/spree_analytics_trackers/store_controller_decorator.rb
@@ -2,12 +2,12 @@ module SpreeAnalyticsTrackers
   module StoreControllerDecorator
     def self.included(base)
       base.include ::Spree::BaseHelper
-      
+
       base.helper 'spree/trackers'
     end
   end
 end
 
-if defined?(Spree::StoreController)
+if defined?(::Spree::StoreController)
   ::Spree::StoreController.include(::SpreeAnalyticsTrackers::StoreControllerDecorator)
 end
