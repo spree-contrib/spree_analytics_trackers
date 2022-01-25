f8d884ed14c5d0e66b951519662da2806e16cc25
Michael Davidson <mdavo6@gmail.com>
2022-01-25 21:39:01 +1100

    Added parameters to gtag config to fix tracking

--- app/overrides/add_google_analytics_page_view_to_spree_application.rb
+++ app/overrides/add_google_analytics_page_view_to_spree_application.rb
@@ -1,9 +1,9 @@
 spree_version = Gem.loaded_specs['spree_core'].version
 unless spree_version >= Gem::Version.create('3.3.0') && spree_version < Gem::Version.create('3.5.0') && spree_version != Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
-    virtual_path: 'spree/layouts/spree_application',
+    virtual_path: 'spree/shared/_head',
     name: 'add_google_analytics_page_viewed_to_spree_application',
-    insert_bottom: 'body',
+    insert_before: 'meta',
     partial: 'spree/shared/trackers/google_analytics/page_viewed.js',
     original: '6841b819babbe4df1f03d0bc8e05dc81bf0d45ad'
   )
