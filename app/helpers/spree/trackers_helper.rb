578f3bb84e726d797475d49db0f24699299fbff0
Michael Davidson <mdavo6@gmail.com>
2022-10-10 12:18:27 +1100

    Added GA product page view

--- app/helpers/spree/trackers_helper.rb
+++ app/helpers/spree/trackers_helper.rb
@@ -25,6 +25,26 @@ module Spree
         hash[:image_url] = default_image_for_product_or_variant(product)
       end.merge(optional).to_json.html_safe
     end
+    
+    def product_for_google(product, optional = {})
+      cache_key = [
+        'spree-google-product',
+        I18n.locale,
+        current_currency,
+        product.cache_key_with_version
+      ].compact.join('/')
+
+      Rails.cache.fetch(cache_key) do
+        {
+          id: product.sku,
+          name: product.name,
+          category: product.category&.name,
+          brand: product.brand&.name,
+          price: product.price_in(current_currency).amount&.to_f,
+          currency: current_currency,
+        }.to_json.html_safe
+      end
+    end
 
     def ga_line_item(line_item)
       variant = line_item.variant
