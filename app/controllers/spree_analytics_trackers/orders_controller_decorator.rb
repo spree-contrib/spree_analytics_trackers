8ddc24079f61c1b4ffeee3b1474975b92e7d5a87
Michael Davidson <mdavo6@gmail.com>
2022-10-11 15:52:31 +1100

    Remove orders controller

--- app/controllers/spree_analytics_trackers/orders_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-module SpreeAnalyticsTrackers
-  module OrdersControllerDecorator
-    def self.included(base)
-      base.helper 'spree/trackers'
-    end
-  end
-end
-
-::Spree::OrdersController.prepend(SpreeAnalyticsTrackers::OrdersControllerDecorator) if ::Spree::OrdersController.included_modules.exclude?(SpreeAnalyticsTrackers::OrdersControllerDecorator)
