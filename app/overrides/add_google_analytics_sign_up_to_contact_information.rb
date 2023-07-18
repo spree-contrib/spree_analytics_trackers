5c981f726c167f364bdb3db79f336673d6df68b7
Michael Davidson <mdavo6@gmail.com>
2023-07-18 16:12:32 +1000

    Added custom events to wholesale sign up process

---
+++ app/overrides/add_google_analytics_sign_up_to_contact_information.rb
@@ -0,0 +1,9 @@
+spree_version = Gem.loaded_specs['spree_core'].version
+unless spree_version >= Gem::Version.create('3.4.0') && spree_version < Gem::Version.create('3.5.0')
+  Deface::Override.new(
+    virtual_path: 'spree/wholesalers/new',
+    name: 'add_google_analytics_sign_up_to_contact_information',
+    insert_bottom: '[data-hook="wholesaler_inside_form"]',
+    partial: 'spree/shared/trackers/google_analytics/sign_up.js'
+  )
+end
