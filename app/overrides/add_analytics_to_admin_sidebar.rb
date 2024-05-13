95ff7934878fd0070ce79f356297672b42b5c8dd
Rahul Singh <81744029+rahul2103@users.noreply.github.com>
2024-05-13 17:11:46 +0530

    spree 4.7 (#72)

--- app/overrides/add_analytics_to_admin_sidebar.rb
+++
@@ -1,8 +0,0 @@
-if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0')
-  Deface::Override.new(
-    virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-    name: 'add_analytics_to_admin_sidebar',
-    insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-    partial: 'spree/admin/shared/analytics_sidebar_menu'
-  )
-end
